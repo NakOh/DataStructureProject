15f5594af862efe4d2442b3ee71d5362df8f9a01
NakOh <kk0704@naver.com>
2014-12-03 01:21:33 +0900

    Orga 가져오기
    막혀있어서 못가져와서 이어서 진행이 안됨..

--- DataStructure/test/auth_test.js
+++ DataStructure/test/auth_test.js
@@ -17,25 +17,46 @@ describe("Auth", function(){
       password: TEST_PASSWORD,
       auth: "basic"
     });
+
     var user = github.getUser();
+
     user.repos(function(err, repos) {
       // console.error(util.inspect(err));
-      console.log(repos);
+      console.log(repos); //user의 레포지터리를 가져온다.
       expect(err).to.be.null;
       repos.should.be.ok;
       repos.length.should.be.above(1);
       done();
 
     });
-    user.orgRepos("CienProject2014", function(err, repos){
 
-      for(var i in repos){
-        $.get(repos[i].contributors_url,function(res){
-          console.log(repos[i]);
-          console.log(res);
+    $(document).on("getRepo",function(e, repo){
+      $.get(repo.contributors_url, function(res){
+        console.log(repo.name);
+        console.log(res);
+        //organization repository에 참여하고 있는 닉네임 추출
+        var arr=[];
+        for(var i in res){
+          arr.push(res[i].login);
+        }
+
+        console.log(arr);
+
+        $.get(res[0].organizations_url, function(org){
+          console.log(org);
         });
+
+      });
+    });
+
+
+    user.orgRepos("CienProject2014", function(err, repos){
+      for(var i in repos){ //repos에는
+        $(document).trigger("getRepo",repos[i]); //CienProject2014라는 orgRepository에 접근 getrepo를 실행시켜라
       }
     });
+
+
 
   });
 });
